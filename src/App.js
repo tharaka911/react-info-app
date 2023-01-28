922a764c365b75c53fede03249b6d1f830fb2bb7
tharaka911 <tharakahitibandara@gmail.com>
2023-01-28 11:08:17 +0530

    files were added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+      <div >
+          <header>
+              <nav>
+                  <img src={"https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg"}  width={200}/>
+              </nav>
+          </header>
+
+          <h1>Fun facts about React...</h1>
+          <ul>
+              <li>Was first released in 2013</li>
+              <li>Was originally created by Jordan Walke</li>
+              <li>Has well over 100K stars on GitHub</li>
+              <li>Is maintained by Facebook</li>
+              <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+      </div>
+  );
+}
+
+export default App;
+
